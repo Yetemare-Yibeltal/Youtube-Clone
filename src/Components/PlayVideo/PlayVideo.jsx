4c22f735f1f4e916f2bc4eb765d4a4e2eb1cb6e5
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-19 16:15:16 +0300

    add playvideo jsx file

---
+++ src/Components/PlayVideo/PlayVideo.jsx
@@ -0,0 +1,126 @@
+import React from 'react'
+import './PlayVideo.css'
+import video1 from '../../assets/video.mp4'
+import like from '../../assets/like.png'
+import dislike from '../../assets/dislike.png'
+import share from '../../assets/share.png'
+import save from '../../assets/save.png'
+import jack from '../../assets/yibeltal_suit.png'
+import user_profile from '../../assets/user_profile.jpg'
+
+const PlayVideo = () => {
+  return (
+    <div className='play-video'>
+      <video src={video1} controls autoPlay muted></video>
+      <h3>Best channel to improve Self improvement </h3>
+      <div className='play-video-info'>
+        <p>5155 Views &bull; 2 days ago</p>
+        <div>
+          <span>
+            <img src={dislike} alt='' />
+            145
+          </span>
+          <span>
+            <img src={share} alt='' />
+            Share
+          </span>
+          <span>
+            <img src={save} alt='' />
+            Save
+          </span>
+        </div>
+      </div>
+      <hr />
+      <div className='publisher'>
+        <img src={jack} alt='' />
+        <div>
+          <p>TruLift Mindsest</p>
+          <span>1M Subscribers</span>
+        </div>
+        <button>Subscribe</button>
+      </div>
+      <div className='vid-description'>
+        <p>Channel taht makes improve your Self confidence</p>
+        <p>Subscribe TrueLift mindset to watch more videos</p>
+        <hr />
+        <h4>130 Comments</h4>
+        <div className='comment'>
+          <img src={user_profile} alt='' />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>there is a big respect to the owner of the videos</p>
+            <div className='comment-action'>
+              <img src={like} alt='' />
+              <span>134</span>
+              <img src={dislike} alt='' />
+            </div>
+          </div>
+        </div>
+        ;
+        <div className='comment'>
+          <img src={user_profile} alt='' />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>there is a big respect to the owner of the videos</p>
+            <div className='comment-action'>
+              <img src={like} alt='' />
+              <span>134</span>
+              <img src={dislike} alt='' />
+            </div>
+          </div>
+        </div>
+        ;
+        <div className='comment'>
+          <img src={user_profile} alt='' />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>there is a big respect to the owner of the videos</p>
+            <div className='comment-action'>
+              <img src={like} alt='' />
+              <span>134</span>
+              <img src={dislike} alt='' />
+            </div>
+          </div>
+        </div>
+        ;
+        <div className='comment'>
+          <img src={user_profile} alt='' />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>there is a big respect to the owner of the videos</p>
+            <div className='comment-action'>
+              <img src={like} alt='' />
+              <span>134</span>
+              <img src={dislike} alt='' />
+            </div>
+          </div>
+        </div>
+        ;
+        <div className='comment'>
+          <img src={user_profile} alt='' />
+          <div>
+            <h3>
+              Jack Nicholson <span>1 day ago</span>
+            </h3>
+            <p>there is a big respect to the owner of the videos</p>
+            <div className='comment-action'>
+              <img src={like} alt='' />
+              <span>134</span>
+              <img src={dislike} alt='' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PlayVideo
