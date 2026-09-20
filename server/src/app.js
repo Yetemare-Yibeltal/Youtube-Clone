7fc814b99ce6adce87b64fc27ee8981c96c15d22
Yibeltal Yitayew <sewmehoneyibeltal540@gmail.com>
2026-09-20 04:10:25 +0300

    Mount user and channel routes

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import morgan from "morgan";
 import { env } from "./config/environment.js";
 import { applySecurity } from "./middleware/security.middleware.js";
+import userRoutes from "./routes/user.routes.js";
+import channelRoutes from "./routes/channel.routes.js";
 import authRoutes from "./routes/auth.routes.js";
 import { apiLimiter } from "./middleware/rateLimit.middleware.js";
 import { notFound } from "./middleware/notFound.middleware.js";
@@ -25,6 +27,8 @@ app.get("/api/health", (req, res) => {
   });
 });
 
+app.use("/api/users", userRoutes);
+app.use("/api/channels", channelRoutes);
 app.use("/api", apiLimiter);
 app.use("/api/auth", authRoutes);
 // Feature routers are mounted here.
